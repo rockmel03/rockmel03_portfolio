3aecb016496eb4bea8b89dca5163e1d94f1849e7
rockmel03 <kamalswag2@gmail.com>
2024-03-27 23:15:58 +0530

    maked about page responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,7 @@ const SkillElement = ({ title, x, y }) => {
       whileInView={{ x: x, y: y }}
       transition={{ duration: 1 }}
       viewport={{ once: true }}
-      className="w-fit font-semibold p-5 bg-dark text-light dark:bg-light dark:text-dark rounded-full absolute cursor-pointer"
+      className="w-fit font-semibold px-3 py-2 sm:p-5  text-light dark:text-dark bg-dark dark:bg-light  rounded-full absolute cursor-pointer"
     >
       {title}
     </motion.h5>
@@ -21,12 +21,17 @@ const SkillElement = ({ title, x, y }) => {
 const About = () => {
   return (
     <main>
-      <Layout className="py-10">
-        <AnimatedHeading data={"Passion Fuels Purpose!"} className="mb-20" />
-        <div className=" flex gap-10 items-start justify-between">
-          <div className="w-[40%]">
-            <h2 className="text-2xl font-semibold">Biography</h2>
-            <p className=" my-3 text-lg font-medium">
+      <Layout className="!py-10">
+        <AnimatedHeading
+          data={"Passion Fuels Purpose!"}
+          className="mb-10 md:mb-20"
+        />
+        <div className="grid grid-cols-8 gap-5 gap-y-10">
+          <div className="col-span-8 sm:col-span-5 md:col-span-5 lg:col-span-4">
+            <h2 className="text-xl md:text-xl lg:text-2xl font-semibold">
+              Biography
+            </h2>
+            <p className=" my-3 text-sm xs:text-base md:text-base lg:text-lg font-medium ">
               Hi i'm Kamal Melkani,a frontend developer , I'm passionate about
               crafting intuitive user interfaces and seamless experiences. With
               expertise in HTML, CSS, and JavaScript, I create visually
@@ -34,18 +39,21 @@ const About = () => {
               adapting to new technologies, I collaborate effectively to deliver
               high-quality solutions that exceed client expectations.
             </p>
-            <p className=" my-3 text-lg font-medium">
+            <p className=" my-3  text-sm xs:text-base md:text-base lg:text-lg font-medium ">
               I have a strong foundation in HTML, CSS, and JavaScript, and I am
               constantly expanding my skills by learning new frameworks and
               technologies.
             </p>
-            <p className=" my-3 text-lg font-medium">
+            <p className=" my-3  text-sm xs:text-base md:text-base lg:text-lg font-medium ">
               My goal is to leverage my skills and passion for frontend
               development to create impactful digital experiences and
               continuously evolve as a professional in this dynamic industry.
             </p>
           </div>
-          <ShadowCard className={` w-[30%] aspect-[3/4] p-10`}>
+          <ShadowCard
+            // className={` max-w-[320px] aspect-[3/4] p-10 col-span-2 `}
+            className={`aspect-[3/4]  col-span-6 col-start-2 row-start-1 sm:row-start-auto sm:col-span-3 md:col-span-3 lg:col-span-2 p-5 md:p-10 `}
+          >
             <div className="w-full h-full rounded-xl bg-yellow-500 overflow-hidden">
               <img
                 src="./dpAi.png"
@@ -54,27 +62,49 @@ const About = () => {
               />
             </div>
           </ShadowCard>
-          <div className="w-[20%] h-full flex flex-col gap-16 items-end justify-between">
+          <div
+            className=" flex lg:flex-col 
+           lg:gap-16 items-center justify-between col-span-8 lg:col-span-2"
+          >
             <div className="flex flex-col items-center gap-5">
-              <h1 className="text-6xl font-black">40+</h1>
-              <h3 className="text-xl font-semibold opacity-80">
+              <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-black">
+                40+
+              </h1>
+              <h3 className="text-sm sm:text-base lg:text-xl text-center font-semibold opacity-80">
+                Satisfied Clients
+              </h3>
+            </div>
+            <div className="flex flex-col items-center gap-5">
+              <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-black">
+                40+
+              </h1>
+              <h3 className="text-sm sm:text-base lg:text-xl text-center font-semibold opacity-80">
+                Satisfied Clients
+              </h3>
+            </div>
+            <div className="flex flex-col items-center gap-5">
+              <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-black">
+                40+
+              </h1>
+              <h3 className="text-sm sm:text-base lg:text-xl text-center font-semibold opacity-80">
                 Satisfied Clients
               </h3>
             </div>
           </div>
         </div>
 
-        <div className=" w-full h-screen mt-20 rounded-full flex items-center justify-center relative bg-radial-light dark:bg-radial-dark">
-          <h3 className="w-fit p-5 bg-dark text-xl font-semibold text-light  dark:bg-light dark:text-dark rounded-full capitalize  hover:scale-95">
+        <div className=" w-full h-[60vh] sm:h-screen mt-20 rounded-full flex items-center justify-center relative bg-radial-light dark:bg-radial-dark sm:bg-radial-light-sm sm:dark:bg-radial-dark-sm md:bg-radial-light-md md:dark:bg-radial-dark-md lg:bg-radial-light-lg lg:dark:bg-radial-dark-lg">
+          <h3 className="w-fit px-4 py-2 sm:p-5 bg-dark text-xl font-semibold text-light  dark:bg-light dark:text-dark rounded-full capitalize  hover:scale-95">
             skills
           </h3>
-          <SkillElement title="HTML" x="-13vw" y=" -30vh" />
-          <SkillElement title="CSS" x="5vw" y=" -33vh" />
-          <SkillElement title="Javascript" x="22vw" y=" -22vh" />
-          <SkillElement title="GSAP" x="26vw" y=" 6vh" />
-          <SkillElement title="ScrollTrigger" x="13vw" y=" 27vh" />
-          <SkillElement title="React.js" x="-22vw" y=" 2vh" />
-          <SkillElement title="Tailwind" x="-13vw" y=" 25vh" />
+          <SkillElement title="HTML" x="-22vw" y=" -20vh" />
+          <SkillElement title="CSS" x="2vw" y=" -30vh" />
+          <SkillElement title="Javascript" x="28vw" y=" -18vh" />
+          <SkillElement title="GSAP" x="30vw" y=" 6vh" />
+          <SkillElement title="ScrollTrigger" x="16vw" y=" 25vh" />
+          <SkillElement title="git & github" x="-1vw" y="35vh" />
+          <SkillElement title="Tailwind" x="-20vw" y=" 22vh" />
+          <SkillElement title="React.js" x="-30vw" y=" 2vh" />
         </div>
       </Layout>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,14 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "radial-light": "repeating-radial-gradient(#0000, #f5f5f5 5px, #fff 100px)",
-        "radial-dark": "repeating-radial-gradient(#fff, #1B1B1B 5px,#1B1B1B 100px)"
+        "radial-light": "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 40px)",
+        "radial-dark": "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 40px)",
+        "radial-light-sm": "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 60px)",
+        "radial-dark-sm": "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 60px)",
+        "radial-light-md": "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 80px)",
+        "radial-dark-md": "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 5px,#1b1b1b 80px)",
+        "radial-light-lg": "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px)",
+        "radial-dark-lg": "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 100px)",
       },
       colors: {
         "light": "#F5F5F5",
